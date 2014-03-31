2a2ae0df3ff2fc4efb89dc8e25803c31a03aab9f
George Rogers <geo@localhost.localdomain>
2014-03-30 21:56:19 -0400

    update

--- lib/rabbid/app.rb
+++ lib/rabbid/app.rb
@@ -1,48 +1,49 @@
 module Rabbid
-  extend self
-  def uuid
-    @uuid ||= SecureRandom.base64
-  end
-  def with v
-    yield v
-  ensure
-    v.close
-  end
-  class App < Sinatra::Base
-    register Sinatra::Async
-    get '/' do ||
-      slim :index
-    end
-    post '/' do ||
-      Rabbid.with Bunny.new do |conn|
-        conn.start
-        ch = conn.create_channel
-        x = ch.fanout("rabbid")
-        x.publish(JSON.dump({number: [Rabbid.uuid, Messages.i[1]+1], text: params[:msg], nick: params[:nick]}))
-        redirect url '/'
-      end
-    end
-    apost '/recv.json' do
-      begin
-        conn = Bunny.new
-        conn.start
-        ch = conn.create_channel
-        q = ch.queue("").bind(ch.fanout("rabbid"))
-        q.subscribe do |d, m, p|
-          begin
-            msg = JSON.parse p
-            body do
-              content_type "application/json"
-              JSON.dump [msg]
-            end
-          ensure
-            conn.close
-          end
-        end
-      rescue
-        conn.close
-        raise $!
-      end
-    end
-  end
+	extend self
+	def uuid
+		@uuid ||= SecureRandom.base64
+	end
+	def with v
+		yield v
+	ensure
+		v.close
+	end
+	class App < Sinatra::Base
+		i = 1
+		register Sinatra::Async
+		get '/' do ||
+			slim :index
+		end
+		post '/' do
+			Rabbid.with Bunny.new do |conn|
+				conn.start
+				ch = conn.create_channel
+				x = ch.fanout("rabbid")
+				x.publish(JSON.dump({number: [Rabbid.uuid, i+1], text: params[:msg], nick: params[:nick]}))
+				redirect url '/'
+			end
+		end
+		apost '/recv.json' do
+			begin
+				conn = Bunny.new
+				conn.start
+				ch = conn.create_channel
+				q = ch.queue("").bind(ch.fanout("rabbid"))
+				q.subscribe do |d, m, p|
+					begin
+						msg = JSON.parse p
+						body do
+							content_type "application/json"
+							JSON.dump [msg]
+						end
+					ensure
+						conn.close
+					end
+				end
+			rescue
+				conn.close
+				raise $!
+			end
+		end
+	end
 end
--- rabbid.gemspec
+++ rabbid.gemspec
@@ -4,30 +4,30 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'rabbid/version'
 
 def add_gem_to spec, g, *reqs
-  spec.add_runtime_dependency g, *reqs
+	spec.add_runtime_dependency g, *reqs
 end
 
 Gem::Specification.new do |spec|
-  spec.name          = "rabbid"
-  spec.version       = Rabbid::VERSION
-  spec.authors       = ["George Rogers"]
-  spec.email         = ["grogers385@gmail.com"]
-  spec.summary       = %q{Write a short summary. Required.}
-  spec.description   = %q{Write a longer description. Optional.}
-  spec.homepage      = ""
-  spec.license       = "MIT"
+	spec.name          = "rabbid"
+	spec.version       = Rabbid::VERSION
+	spec.authors       = ["George Rogers"]
+	spec.email         = ["grogers385@gmail.com"]
+	spec.summary       = %q{Write a short summary. Required.}
+	spec.description   = %q{Write a longer description. Optional.}
+	spec.homepage      = ""
+	spec.license       = "MIT"
 
-  spec.files         = `git ls-files -z`.split("\x0")
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
-  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+	spec.files         = `git ls-files -z`.split("\x0")
+	spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+	spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+	spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.5"
-  spec.add_development_dependency "rake"
+	spec.add_development_dependency "bundler", "~> 1.5"
+	spec.add_development_dependency "rake"
 
-  add_gem_to spec, 'sinatra', '~> 1.4.0'
-  add_gem_to spec, 'async_sinatra'
-  add_gem_to spec, 'slim', '~> 2.0.2'
-  add_gem_to spec, 'thin'
-  add_gem_to spec, 'bunny', '~> 1.1.8'
+	add_gem_to spec, 'sinatra', '~> 1.4.0'
+	add_gem_to spec, 'async_sinatra'
+	add_gem_to spec, 'slim', '~> 2.0.2'
+	add_gem_to spec, 'thin'
+	add_gem_to spec, 'bunny', '~> 1.1.8'
 end
